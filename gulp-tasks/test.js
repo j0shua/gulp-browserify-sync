188f6a94f94c239de8d0555f66e3f3eacb22132c
j0shua <joshua.j.goldsmith@gmail.com>
2015-01-08 00:09:18 -0500

    added source-maps + uglify

--- gulp-tasks/test.js
+++ gulp-tasks/test.js
@@ -1,7 +1,14 @@
+// check out:
+// https://github.com/gulpjs/gulp/blob/master/docs/recipes/browserify-uglify-sourcemap.md
+// for using package.json.version to control name
+//
 module.exports = function(baseDir){
     var gulp = require('gulp'),
         browserify = require('browserify'),
-        source = require('vinyl-source-stream');
+        source = require('vinyl-source-stream'),
+        buffer = require('vinyl-buffer'),
+        uglify = require('gulp-uglify'),
+        sourcemaps = require('gulp-sourcemaps');
 
 
     gulp.task('default', function(){
@@ -12,9 +19,16 @@ module.exports = function(baseDir){
 
 
     gulp.task('js', function(){
-        return browserify('./scripts/app.js')
+        return browserify({
+                entries: ['./scripts/app.js'],
+                debug: true
+            })
             .bundle()
             .pipe(source('bundle.js'))
+            .pipe(buffer())
+            .pipe(sourcemaps.init({ loadMaps: true }))
+                .pipe(uglify())
+            .pipe(sourcemaps.write('./'))
             .pipe(gulp.dest('./build/'));
     });
 };
